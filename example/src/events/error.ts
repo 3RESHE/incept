e6f64ac38bf10cd49d9f8e8900b7f12d7e181c57
Chris Blanquera <chris@incept.asia>
2024-12-07 23:54:49 +0800

    error handling on build files

--- example/src/events/error.ts
+++ example/src/events/error.ts
@@ -6,7 +6,7 @@ import type { ServerRequest } from '@stackpress/ingest/dist/types';
 import type Response from '@stackpress/ingest/dist/Response';
 import type { TemplatePlugin } from '@stackpress/incept-ink/dist/types';
 
-export default async function Errorage(req: ServerRequest, res: Response) {  
+export default async function ErrorPage(req: ServerRequest, res: Response) {  
   //get the server
   const server = req.context;
   //get the renderer
@@ -22,9 +22,17 @@ export default async function Errorage(req: ServerRequest, res: Response) {
 
     return { ...trace, source: fs.readFileSync(file, 'utf8') };
   });
-  //show form
-  res.setHTML(await render('@/templates/500', { 
+  //render the template
+  const html = await render('@/templates/500', { 
     ...response, 
     url: req.url.pathname 
-  }));
+  });
+  if (req.url.pathname.endsWith('.js')) {
+    res.setBody('text/javascript', `document.write(${JSON.stringify(html)});`);
+    return;
+  } else if (req.url.pathname.endsWith('.css')) {
+    res.setBody('text/css', `/* ${response.error} */`);
+    return;
+  }
+  res.setHTML(html);
 };
