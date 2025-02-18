230ec12a614b48efb308077c00592a4062d01027
Chris Blanquera <chris@incept.asia>
2025-02-18 13:56:18 +0800

    version bump

--- example/src/modules/store/index.ts
+++ example/src/modules/store/index.ts
@@ -6,12 +6,13 @@ import pg from '@stackpress/inquire-pg';
  * This is setup this way so it's consistent.
  */
 export default async function make() {
-  //this is the raw resource, anything you want
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    application_name: "$ docs_simplecrud_iam",
-  });
-  const connection = await pool.connect();
   //this maps the resource to the engine
-  return pg(connection);
+  return pg(() => {
+    //this is the raw resource, anything you want
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      application_name: "$ docs_simplecrud_iam",
+    });
+    return pool.connect();
+  });
 }
