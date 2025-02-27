67854ee7162f635d957ab7ffdff0b6777252cc5e
Chris Blanquera <chris@incept.asia>
2025-02-27 13:55:44 +0800

    updating render to better case for production environments

--- packages/incept-ink/src/plugin.ts
+++ packages/incept-ink/src/plugin.ts
@@ -4,12 +4,68 @@ import path from 'node:path';
 import type { CLIProps } from '@stackpress/idea-transformer/dist/types';
 import type Transformer from '@stackpress/idea-transformer/dist/Transformer';
 import type { IM, SR } from '@stackpress/ingest/dist/types';
+import type { ServerDocumentClass, InkCompiler } from '@stackpress/ink/dist/types';
 import type Server from '@stackpress/ingest/dist/Server';
+import FileLoader from '@stackpress/lib/dist/system/FileLoader';
+import ink, { cache, serialize } from '@stackpress/ink/compiler';
 import RefreshServer from '@stackpress/ink-dev/dist/RefreshServer';
-import ink, { cache } from '@stackpress/ink/compiler';
 import { plugin as css } from '@stackpress/ink-css';
 //local
 import type { InkPlugin } from './types';
+
+/**
+ * Determine the id (without component because it will check to see if it exists)
+ */
+export function id(filePath: string, loader: FileLoader) {
+  //get the absolute path
+  const absolute = loader.absolute(filePath);
+  let relative = path.relative(loader.cwd, absolute);
+  relative = relative.startsWith('.') ? relative : `./${relative}`;
+  //if this is a node modules
+  if (relative.includes('node_modules/')) {
+    //remove the left side of the path
+    relative = relative.split('node_modules/')[1];
+  }
+  return serialize(relative);
+};
+
+/**
+ * Makes a custom render function
+ */
+export function makeRenderer(
+  development: boolean, 
+  serverPath: string, 
+  compiler: InkCompiler, 
+  refresh: RefreshServer
+) {
+  //make a new file loader (for render function below)
+  const loader = new FileLoader(compiler.fs, compiler.config.cwd);
+  return function render(
+    filePath: string, 
+    props: Record<string, unknown> = {}
+  ) {
+    if (!path.extname(filePath)) {
+      filePath = `${filePath}.ink`;
+    }
+    if (development) {
+      refresh.sync(compiler.fromSource(filePath));
+      return compiler.render(filePath, props);
+    }
+    //if production use cache only (return the markup)
+    //get the server build path
+    const server = path.join(serverPath, `${id(filePath, loader)}.js`);
+    //if the server file does not exist, try to dynamic render
+    if (!loader.fs.existsSync(server)) {
+      return compiler.render(filePath, props);
+    }
+    //get the build object
+    const build = require(server);
+    const Document = build.default as ServerDocumentClass;
+    const document = new Document();
+    //render the document
+    return document.render(props);
+  };
+}
 
 /**
  * This interface is intended for the Incept library.
@@ -21,6 +77,7 @@ export default function plugin(server: Server) {
     const config = server.config.withPath;
     //get server environment
     const environment = config.get<string>('server.mode') || 'development';
+    const development = environment === 'development';
     //get template engine config
     const cwd = config.get<string>('template.config.cwd') || process.cwd();
     const minify = config.get<boolean>('template.config.minify');
@@ -38,22 +95,11 @@ export default function plugin(server: Server) {
     //use ink css
     compiler.use(css());
     //use build cache
-    if (environment !== 'development') {
+    if (!development) {
       compiler.use(cache({ clientPath, serverPath, manifestPath }));
     }
-    //common render function
-    const render = function(
-      filePath: string, 
-      props: Record<string, unknown> = {}
-    ) {
-      if (!path.extname(filePath)) {
-        filePath = `${filePath}.ink`;
-      }
-      if (environment === 'development') {
-        refresh.sync(compiler.fromSource(filePath));
-      }
-      return compiler.render(filePath, props);
-    };
+    //make a render function
+    const render = makeRenderer(development, serverPath, compiler, refresh);
     //add ink as a project plugin
     server.register('ink', { compiler, refresh, render, templates });
     //add renderer to the project
